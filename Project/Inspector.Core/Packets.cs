bc3e991a7cb1aa7f3acc8ac6eba0a9153f429d40
R_Bence <bencerazso@gmail.com>
2026-05-05 19:56:04 +0200

    traffic-logger

--- Project/Inspector.Core/Packets.cs
+++ Project/Inspector.Core/Packets.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Reflection;
 
 using PacketDotNet;
@@ -13,17 +14,20 @@ public class Packets : IDisposable
     private LibPcapLiveDevice _device; 
     private bool _isDisposed = false;
     private int _db = 0;
-    public Packets()
+    
+    private readonly trafficLogger tl;
+    public Packets(trafficLogger rmaker)
     {
         Console.WriteLine("Constructor");
         _device = LibPcapLiveDeviceList.Instance[0];
+        this.tl = rmaker;
     }
 
 
 
-    public void packetStartCapture()
+    public async void packetStartCapture()
     {
-        Console.WriteLine("Start capture");
+        Debug.WriteLine("Start capture");
         _device.Open();
         _device.OnPacketArrival += Device_OnPacketArrival;
         
@@ -32,19 +36,35 @@ public class Packets : IDisposable
 
     public void packetStopCapture()
     {
-        Console.WriteLine("Stop capture");   
+        Debug.WriteLine("Stop capture");   
         _device.StopCapture();
     }
     
     public void Device_OnPacketArrival(object s, PacketCapture e)
     {
         var pack = Packet.ParsePacket(e.GetPacket().LinkLayerType, e.GetPacket().Data);
+        /*
+        Console.WriteLine(pack);
+        */
         if (pack == null) return;
         var time = DateTimeOffset.Now;
         var ipPacket = pack.Extract<IPPacket>();
+        if (ipPacket == null) return;
         Console.WriteLine("--------------------------");
-        Console.WriteLine("{0} -- {1}:{2}:{3}:{4} | {5}",_db, time.Hour, time.Minute, time.Second, time.Millisecond, ipPacket);    
-        _db++;
+        Console.WriteLine("{0} -- {1}:{2}:{3}:{4} | {5}",_db, time.Hour, time.Minute, time.Second, time.Millisecond, ipPacket);
+        try
+        {
+            var packetString = ipPacket.ToString();
+
+            Task.Run(() => tl.write(packetString));
+            _db++;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+
+
     }
     
     public void Dispose()
@@ -53,7 +73,6 @@ public class Packets : IDisposable
         {
             _device.StopCapture();
             _device.Close();
-            this.Dispose();
             _isDisposed = true;
         }  
     }
