854b798b7ad164e78a4bb0015744942a8f89a1fb
R_Bence <bencerazso@gmail.com>
2026-05-16 22:41:27 +0200

    fix core and port scan

--- Project/Inspector.Core/ruleEngine.cs
+++ Project/Inspector.Core/ruleEngine.cs
@@ -1,49 +1,84 @@
+using System.Diagnostics;
+
+using PacketDotNet;
+
 namespace Inspector.Core;
 
-public class ruleEngine
+public class RuleEngine
 {
     private bool _rule1on { get; set; } = false;
     private bool _rule2on { get; set; } = false;
     private bool _rule3on { get; set; } = true;
     
-    ruleEngine()
-    {
-    }
+    public RuleEngine() { }
 
-    // port scan detect
-    public bool IsRule1On(List<PacketData> packets)
+    public bool PortScanDetect(ref List<PacketData> rawPackets)
     {
-        foreach (var packet in packets)
+        List<string> vizsgaltIP = new List<string>();
+        foreach (var v in rawPackets)
         {
+            if (vizsgaltIP.Contains(v.SourceAddress))
+            {
+                continue;
+            }
+            int db = 0;
+            List<string> port = new List<string>();
             
+            foreach (var k in rawPackets)
+            {
+                if (v.SourceAddress == k.SourceAddress && !port.Contains(k.DestinationPort))
+                {
+                    port.Add(k.DestinationPort);
+                    var dif = DateTime.Parse(k.Time) - DateTime.Parse(v.Time);
+                    if (dif <= TimeSpan.Parse("00:00:05"))
+                    {
+                        db++;
+                    }
+                } 
+            }
+            vizsgaltIP.Add(v.SourceAddress);
+
+            if (db > 10)
+            {
+                foreach (var rPacket in rawPackets)
+                {
+                    if (v.SourceAddress == rPacket.SourceAddress)
+                    {
+                        rPacket.PotentialDanger = true;
+                        rPacket.PotentialDangerMessage = "Port scan";
+                        Debug.WriteLine("Danger!!!");
+                    }
+                }
+            }
+
         }
+
+        return false;
     }
 
     // syn - ack
-    public bool IsRule2On(List<PacketData> packets)
+    public bool IsRule2On(ref List<PacketData> packets)
     {
         return false;
     }
 
-    // header length
-    public (string, bool) headerLengthCheck(PacketData packet)
+    public bool HeaderLengthCheck(ref PacketData packet)
     {
-        if (packet.HeaderLength * 4 <= 20)
+        if (packet.HeaderLength * 4 < 20)
         {
-            return ("Not enough", true);
+            Debug.WriteLine("Danger!!!");
+            packet.PotentialDanger = true;
+            packet.PotentialDangerMessage = "Too short";
+            return true;
         }
-        else if (packet.HeaderLength * 4 >= 24 && packet.HeaderLength * 4 <= 60)
+        if (packet.HeaderLength * 4 > 60)
         {
-            return ("warrning", true);
+            Debug.WriteLine("Danger!!!");
+            packet.PotentialDanger = true;
+            packet.PotentialDangerMessage = "Too much";
+            return true;
         }
-        else if (packet.HeaderLength * 4 >= 60)
-        {
-            return ("Too much", true);
-        }
-        else
-        {
-            return ("OK", false);
-        }
-        
+
+        return false;
     }
 }
